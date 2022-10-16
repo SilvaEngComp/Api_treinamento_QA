a17c0c8475e36eed1e5d7eac5ba0feb1be73338e
SilvaEngComp <silvaengcomp@gmail.com>
2022-10-16 17:06:45 -0300

    adicionando obrigatoreidade de autenticação para rota v2

--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function () {
-    Route::get('auth/codeRescue/{code}', [AuthController::class, 'codeValidation']);
-    Route::get('auth/recoverAccess/{email}', [AuthController::class, 'checkEmailExistente']);
-    Route::patch('/auth/updatePassword/user/{user}', [AuthController::class, 'updatePassword']);
+    // Route::get('auth/codeRescue/{code}', [AuthController::class, 'codeValidation']);
+    // Route::get('auth/recoverAccess/{email}', [AuthController::class, 'checkEmailExistente']);
+    // Route::patch('/auth/updatePassword/user/{user}', [AuthController::class, 'updatePassword']);
     Route::post('/auth/login', [AuthController::class, 'login']);
 });
 
 
-Route::prefix('v2')->group(function () {
-
-    Route::prefix('netEmpresa')->group(function () {
-        Route::get("/", [UserController::class, "index"]);
-        Route::get("/{user}", [UserController::class, "show"]);
-        Route::post("/", [UserController::class, "store"]);
-        Route::patch("/{user}", [UserController::class, "update"]);
-        Route::delete("/{user}", [UserController::class, "destroy"]);
-    });
-});
+Route::prefix('v2')->group(
+    function () {
+        Route::group(['middleware' => ['auth:sanctum']], function () {
+            Route::prefix('netEmpresa')->group(function () {
+                // Route::get("/", [UserController::class, "index"]);
+                // Route::get("/{user}", [UserController::class, "show"]);
+                Route::post("/", [UserController::class, "store"]);
+                // Route::patch("/{user}", [UserController::class, "update"]);
+                // Route::delete("/{user}", [UserController::class, "destroy"]);
+            });
+        });
+    }
+);
